e5b4925a7d656fef163fb4ec863a09dd0e8b3ad3
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-07-20 16:44:19 +0600

    The header section updated

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${outfit.variable} ${ovo.variable}`}>
+    <html lang="en" className="scroll-smooth"> 
+      <body className={`${outfit.variable} ${ovo.variable} antialiased leading-8 overflow-x-hidden`}>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 'use client';
 
 import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import About from "./Components/About"
 
 export default function Home() {
   return (
    <>
    <Navbar/>
+   <Header />
+   <About />
    </>
   );
 }
