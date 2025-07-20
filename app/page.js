9231fdb31a5945f0efb2d83f648428963e3142f6
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-07-20 20:33:51 +0600

    Service section updated

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
 import About from "./Components/About"
+import Services from "./Components/Services";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
    <Navbar/>
    <Header />
    <About />
+   <Services />
    </>
   );
 }
